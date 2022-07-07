101fee45e9c8107dccaf710249a84da3f654a5aa
Mahdi Nikdan <nikdanmahdi@gmail.com>
2022-07-07 20:06:09 +0430

    debug

--- conv_all_layers.py
+++ conv_all_layers.py
@@ -58,7 +58,7 @@ class Args:
         self.initial_epoch = 0
         self.int_steps = 7
         self.int_downsize = 2
-        self.run_name = 'conv_all_layers_lr001'
+        self.run_name = 'test'
         self.model_dir = './trained-models/torch/' + self.run_name + '/'
 
 
